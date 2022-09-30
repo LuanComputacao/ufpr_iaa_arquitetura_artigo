f8236e796ed9b25535094e9e1e1593ce0c2af6aa
LuanComputacao <eng.luanroger@gmail.com>
2022-09-29 22:05:46 -0300

    feat: apply project structure

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import pandas as pd
+import pandas_profiling
+
 from kdd import df_body_fat, heat_map_analysis, show_attibute_relations
 
+SEPARATOR = '-' * 80
 
 def main():
-    show_attibute_relations(df_body_fat, 'BodyFat')
-    heat_map_analysis(df_body_fat)
+    df = df_body_fat
+    print(df.dtypes)
+    print(df.shape)
+    print(df.describe())
+    print(SEPARATOR )
+    print(df.isnull().sum())
+    print(SEPARATOR )
+    density = df.Density
+    print(density.head(3))
+    print(SEPARATOR )
+    df = pd.get_dummies(df)
+    print(df.columns)
+    # profile = pandas_profiling.ProfileReport(df_body_fat, title="Pandas Profiling Report")
+    # profile.to_file("report.html")
+    # show_attibute_relations(df_body_fat, 'BodyFat')
+    # heat_map_analysis(df_body_fat)
 
 
 if __name__ == '__main__':
